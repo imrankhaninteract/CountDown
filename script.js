b6f6404b7782c8b41b35f61e0c4bb89d3b5258dc
imrankhan <imrankhaninteract@gmail.com>
2024-01-03 20:34:43 +0600

    secondCommit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const endDate = "27 July 2022 08:20:00 PM"
+const endDate = "27th March 2028 08:20:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
